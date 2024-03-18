cf4a35b1e061508b302585411dd860b4df748e3f
Broderick <zerothesu@gmail.com>
2024-03-17 23:03:21 -0600

    started Beautiful-Day-At-The-Movies

---
+++ Beautiful-Dat-At-The-Movies.js
@@ -0,0 +1,14 @@
+let x = 20
+let y = 23
+let d = 6
+
+function beautifulDays(i,j,k){
+    console.log(Math.abs(i-j))
+    let loopCount = Math.abs(i-j)
+    let flippedNum
+    for(let i=0;i<loopCount;i++){
+        
+    }
+}
+
+beautifulDays(x,y,d)
