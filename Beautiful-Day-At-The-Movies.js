7db7fedf4e3edc2c995a37b0baeeb5d320df7c48
Broderick <zerothesu@gmail.com>
2024-03-20 18:33:38 -0600

    did some more on the beautiful day at the movies challenge

--- Beautiful-Day-At-The-Movies.js
+++ Beautiful-Day-At-The-Movies.js
@@ -1,17 +1,34 @@
-let x = 20
-let y = 23
-let d = 6
+let x = 49860
+let y = 205494
+let d = 155635764
 
 function beautifulDays(x,y,k){
-    console.log(Math.abs(x-y))
+    // console.log(Math.abs(x-y))
     let loopCount = Math.abs(x-y)
     let xLength = x.toString().length
-    let flippedNum;
+    let flippedNum = ''
+    let zerosGone = false
+    let beautifulDays = 0
     for(let i=0;i<=loopCount;i++){
-        for(let i=xLength; i>0; i--){
-            console.log('flip')
+        for(let f=xLength; f>0; f--){
+            // console.log((x + i).toString()[f - 1])
+            if(((x + i).toString()[f -1]) != '0'){
+                // console.log(`the num is ${((x + i).toString()[f -1])}`)
+                flippedNum += ((x + i).toString()[f -1])
+                zerosGone = true
+            }else if(zerosGone && ((x + i).toString()[f -1]) == '0'){
+                flippedNum += ((x + i).toString()[f -1])
+            }
         }
+        // console.log(`flipped num ${flippedNum}`)
+        // console.log('-------')
+        if((((x + i)-(parseInt(flippedNum)))%k) === 0){
+            beautifulDays += 1
+            // console.log('beautiful day')
+        }
+        flippedNum = ''
     }
+    return beautifulDays
 }
 
-beautifulDays(x,y,d)
+console.log(beautifulDays(x,y,d))
