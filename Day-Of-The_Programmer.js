06f01aef595be6a8540aed09ec2d34a99d5d362b
Broderick <zerothesu@gmail.com>
2024-04-03 14:30:47 -0600

    removed useless comment

--- Day-Of-The_Programmer.js
+++ Day-Of-The_Programmer.js
@@ -1,18 +1,3 @@
-/*
-jan - 31
-feb - 28 leap(divisible 4) 29
-march - 31
-april - 30
-may - 31
-june - 30
-july - 31
-august - 31
-september - 30
-october - 31
-november - 30
-december - 31
-*/
-
 function dayOfProgrammer(year) {
     let dayOfProgrammerDate
 
