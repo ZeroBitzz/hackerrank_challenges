75703c9b79c48977902f27eeedabe62857e8ffb6
Broderick <zerothesu@gmail.com>
2024-04-03 14:29:40 -0600

    cleaned up day of the programmer challenge

--- Day-Of-The_Programmer.js
+++ Day-Of-The_Programmer.js
@@ -18,18 +18,10 @@ function dayOfProgrammer(year) {
 
     // if the year is before 1918 then the julian calender rules for leap years take place
     if(year > 1918){
-        if((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)){
-            dayOfProgrammerDate = `12.09.${year}`
-        }else{
-            dayOfProgrammerDate = `13.09.${year}`
-        }
+        (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0) ? dayOfProgrammerDate = `12.09.${year}` : dayOfProgrammerDate = `13.09.${year}`
     // if the year is after 1918 then the gregorian calender rules for leap years take place
     }else if(year < 1918){
-        if(year % 4 === 0){
-            dayOfProgrammerDate = `12.09.${year}`
-        }else{
-            dayOfProgrammerDate = `13.09.${year}`
-        }
+        year % 4 === 0 ? dayOfProgrammerDate = `12.09.${year}` : dayOfProgrammerDate = `13.09.${year}`
     // if the year is not greater or less than 1918, then it is 1918 and the 256th day is already known
     }else{
         dayOfProgrammerDate = `26.09.${year}`
