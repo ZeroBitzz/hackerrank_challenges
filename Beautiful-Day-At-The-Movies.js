f45e1a5628a1794be73dfc9fb30a5958fde17135
Broderick <zerothesu@gmail.com>
2024-04-03 14:20:33 -0600

    cleaned up code like a boss

--- Beautiful-Day-At-The-Movies.js
+++ Beautiful-Day-At-The-Movies.js
@@ -1,19 +1,11 @@
 let x = 49860 
 let y = 205494 
 let d = 155635764
-console.log(Number.isInteger(0))
-console.log(Number.isInteger(-1))
 function beautifulDays(x,y,k){
-    let loopCount = Math.abs(x-y)
-    let zerosGone
     let beautifulDays = 0
-    for(let i=0;i<=loopCount;i++){
-        zerosGone = false
-        let flippedNum=(x+i).toString().split('').reverse().join('')
-        // console.log(`flipped num is ${flippedNum}`)
-        if(Number.isInteger(Math.abs(((x+i)-flippedNum)/k))){
-            beautifulDays += 1
-        }
+    for(let i=0;i<=(Math.abs(x-y));i++){
+        let flippedValue=(x+i).toString().split('').reverse().join('')
+        Number.isInteger(Math.abs(((x+i)-flippedValue)/k)) ? beautifulDays += 1 : null
     }
     return beautifulDays
 }
