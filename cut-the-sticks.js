aa60120c88236bfc7a66001ed1bfaeaf48fa7a51
Broderick <zerothesu@gmail.com>
2024-08-19 00:12:28 -0600

    made some more progress on cut-the-sticks

--- cut-the-sticks.js
+++ cut-the-sticks.js
@@ -1,12 +1,25 @@
 function cutTheSticks(arr) {
-    let stickCountArr = [arr.length]
+    let stickCountArr = []
     let newArr = arr
-    while(arr.length != 0){
+    let loopCount = arr[0]
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] > loopCount){
+            loopCount = arr[i]
+        }
+    }
+    console.log(`loopcount: ${loopCount}`)
+    for(let i=0; i<loopCount; i++){
+        let tempArr = []
         for(let i=0; i<newArr.length; i++){
-            if(arr[i] > 1){
-                arr[i] -= 1
+            if(newArr[i] !== 1){
+                newArr[i] -= 1
+                tempArr.push(newArr[i])
             }
         }
+        stickCountArr.push(tempArr.length)
     }    
-
+    console.log(stickCountArr)
 }
+
+testArr = [5,4,4,2,2,8]
+cutTheSticks(testArr)
