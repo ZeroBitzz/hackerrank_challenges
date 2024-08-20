4c272fd71e93e06a94c6ed0de953078c4c2ddd8c
Broderick <zerothesu@gmail.com>
2024-08-20 12:46:25 -0600

    tried recursion to solve problem, coming back later

--- cut-the-sticks.js
+++ cut-the-sticks.js
@@ -1,34 +1,36 @@
 function cutTheSticks(arr) {
-    let sticksLeftArr = [arr.length]
-    let newArr = []
+    let stickLengthArr = [arr.length]
     let smallestStick = arr[0]
-    for(let x=1; x<arr.length; x++){
-        if(arr[x] < smallestStick){
-            smallestStick = arr[x]
-        }
-    }
-    while(true){
-        newArr = []
-        for(let i=0; i<arr.length; i++){
-            if(arr[i] > 1){
-                arr[i] -= smallestStick
-                newArr.push(arr[i])
+    if(arr.length > 1){
+        console.log('array length')
+        console.log(arr.length)
+        console.log("----------")
+        
+        // gets smallest stick
+        for(let i=1; i<arr.length; i++){
+            if(arr[i] < smallestStick){
+                smallestStick = arr[i]
             }
         }
-        if(newArr.length === 0){
-            break
-        }
-        for(let y=1; y<newArr.length; y++){
-            if(newArr[y] < smallestStick){
-                smallestStick = newArr[y]
+
+        for(let x=0; x<arr.length; x++){
+            if((arr[x] - smallestStick) > 0){
+                arr[x] -= smallestStick
+            }else{
+                arr.splice(arr.indexOf(arr[x]), 1)
             }
+            console.log("newarr")
+            console.log(arr)
+            console.log('--------')
         }
-        sticksLeftArr.push(newArr.length)
+        stickLengthArr.push(arr.length)
+        cutTheSticks(arr)
+    }else{
+        return
     }
-    console.log(sticksLeftArr)
-    return sticksLeftArr
+    
 }
 
-testArr = [5,4,4,2,2,8]
-// testArr = [1,2,3,4,3,3,2,1]
+// testArr = [5,4,4,2,2,8]
+testArr = [1,2,3,4,3,3,2,1]
 cutTheSticks(testArr)
