034c03317a29b3ad67999e67ad56cd5a48e6ea73
Broderick <zerothesu@gmail.com>
2024-08-25 23:22:28 -0600

    cut-the-sticks

--- cut-the-sticks.js
+++ cut-the-sticks.js
@@ -5,13 +5,13 @@ function cutTheSticks(arr) {
     while(arr.length > 0){
         rewriterArr = []
         // gets smallest stick
-        for(let i=0; i<arr.length; i++){
-            if(arr[0] < smallestStick){
-                smallestStick = arr[0]
+        for(let i=1; i<arr.length; i++){
+            if(arr[i] < smallestStick){
+                smallestStick = arr[i]
             }
         }
-        console.log(`The current array: ${arr}`)
-        console.log(`Smallest Stick ${smallestStick}`)
+        // console.log(`The current array: ${arr}`)
+        // console.log(`Smallest Stick ${smallestStick}`)
         // subtracts from each stick
         for(let x=0; x<arr.length; x++){
             arr[x] -= smallestStick
@@ -24,12 +24,12 @@ function cutTheSticks(arr) {
         if(arr.length > 0){
             stickLengthArr.push(arr.length)
         }
-        console.log(`New arr: ${arr}`)
-        console.log('-----------')
+        // console.log(`New arr: ${arr}`)
+        // console.log('-----------')
     }
     return stickLengthArr
 }
 
-// testArr = [5,4,4,2,2,8]
-testArr = [1,2,3,4,3,3,2,1]
+testArr = [5,4,4,2,2,8]
+// testArr = [1,2,3,4,3,3,2,1]
 console.log(cutTheSticks(testArr))
