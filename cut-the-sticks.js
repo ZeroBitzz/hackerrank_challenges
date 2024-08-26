9fd21e9cee584be26e8d6cd899a9b2ed0c303aab
Broderick <zerothesu@gmail.com>
2024-08-25 23:17:07 -0600

    cut-the-sticks

--- cut-the-sticks.js
+++ cut-the-sticks.js
@@ -1,36 +1,35 @@
 function cutTheSticks(arr) {
     let stickLengthArr = [arr.length]
     let smallestStick = arr[0]
-    if(arr.length > 1){
-        console.log('array length')
-        console.log(arr.length)
-        console.log("----------")
-        
+    let rewriterArr = []
+    while(arr.length > 0){
+        rewriterArr = []
         // gets smallest stick
-        for(let i=1; i<arr.length; i++){
-            if(arr[i] < smallestStick){
-                smallestStick = arr[i]
+        for(let i=0; i<arr.length; i++){
+            if(arr[0] < smallestStick){
+                smallestStick = arr[0]
             }
         }
-
+        console.log(`The current array: ${arr}`)
+        console.log(`Smallest Stick ${smallestStick}`)
+        // subtracts from each stick
         for(let x=0; x<arr.length; x++){
-            if((arr[x] - smallestStick) > 0){
-                arr[x] -= smallestStick
-            }else{
-                arr.splice(arr.indexOf(arr[x]), 1)
+            arr[x] -= smallestStick
+            if(arr[x] >= 1){
+                rewriterArr.push(arr[x])
             }
-            console.log("newarr")
-            console.log(arr)
-            console.log('--------')
         }
-        stickLengthArr.push(arr.length)
-        cutTheSticks(arr)
-    }else{
-        return
+        // set the array to the new values with no zeros or negatives then push length to stickLengthArr
+        arr = rewriterArr
+        if(arr.length > 0){
+            stickLengthArr.push(arr.length)
+        }
+        console.log(`New arr: ${arr}`)
+        console.log('-----------')
     }
-    
+    return stickLengthArr
 }
 
 // testArr = [5,4,4,2,2,8]
 testArr = [1,2,3,4,3,3,2,1]
-cutTheSticks(testArr)
+console.log(cutTheSticks(testArr))
