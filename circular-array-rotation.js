1d3d66e17d94cab8c8d72b570f63a3e08fd5c636
Broderick <zerothesu@gmail.com>
2024-08-27 15:14:56 -0600

    ninja bug

---
+++ circular-array-rotation.js
@@ -0,0 +1,32 @@
+function circularArrayRotation(a, k, queries) {
+    let savedInt
+    let rewriterArr = a
+    // first for loop if for shifting array "k" many times to the right
+    for(let i=0; i<k; i++){
+        console.log(`rewriter array: ${rewriterArr}`)
+        // second for loop if for setting the new values of the shifted array 
+        for(let x=0; x<a.length; x++){
+            if(a[x - 1] === undefined){
+                console.log('first')
+                a[x] = rewriterArr[rewriterArr.length - 1]
+                console.log(a)
+            }else if(a[x + 1] === undefined){
+                console.log('last')
+                a[x] = rewriterArr[0]
+                console.log(a)
+            }else{
+                console.log('lettuce and tomatoes')
+                console.log(`setting ${a[x]} from ${a} to ${rewriterArr[x - 1]} from ${rewriterArr}`)
+                a[x] = rewriterArr[x - 1]
+                console.log(a)
+
+            }
+        }
+        rewriterArr = a
+    }
+    console.log(rewriterArr)
+}
+
+arr = [3,5,6]
+
+circularArrayRotation(arr, 1, 0)
