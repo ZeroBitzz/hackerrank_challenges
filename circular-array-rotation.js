c923e5bcd92d80dcc65e6aa0eabcc8c4caa08c59
Broderick <zerothesu@gmail.com>
2024-08-29 01:02:58 -0600

    got circular-array-rotation working but not fast enough for hacker rank

--- circular-array-rotation.js
+++ circular-array-rotation.js
@@ -1,32 +1,38 @@
 function circularArrayRotation(a, k, queries) {
     let savedInt
-    let rewriterArr = a
+    let rewriterArr = []
+    let queriesArr = []
+    //sets rewriter array
+    for(let y=0; y<a.length; y++){
+        rewriterArr.push(a[y])
+    }
     // first for loop if for shifting array "k" many times to the right
     for(let i=0; i<k; i++){
-        console.log(`rewriter array: ${rewriterArr}`)
         // second for loop if for setting the new values of the shifted array 
         for(let x=0; x<a.length; x++){
             if(a[x - 1] === undefined){
-                console.log('first')
                 a[x] = rewriterArr[rewriterArr.length - 1]
-                console.log(a)
             }else if(a[x + 1] === undefined){
-                console.log('last')
-                a[x] = rewriterArr[0]
-                console.log(a)
+                a[x] = rewriterArr[rewriterArr.length - 2]
             }else{
-                console.log('lettuce and tomatoes')
-                console.log(`setting ${a[x]} from ${a} to ${rewriterArr[x - 1]} from ${rewriterArr}`)
                 a[x] = rewriterArr[x - 1]
-                console.log(a)
 
             }
         }
-        rewriterArr = a
+        // resets rewriter array
+        rewriterArr = []
+        for(let y=0; y<a.length; y++){
+            rewriterArr.push(a[y])
+        }
     }
-    return a
+
+    // gets the value of the queries and returns
+    for(let g=0; g<queries.length; g++){
+        queriesArr.push(a[queries[g]])
+    }
+    return queriesArr
 }
 
-arr = [3,5,6]
+arr = [3,5,9,7]
 
-console.log(circularArrayRotation(arr, 1, 0))
+console.log(circularArrayRotation(arr, 1, [1,2]))
