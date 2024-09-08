a0fc1a77e38e723cbeb9e86f0e9b67732175c92a
Broderick <zerothesu@gmail.com>
2024-09-08 16:22:39 -0600

    circular array rotation

--- circular-array-rotation.js
+++ circular-array-rotation.js
@@ -14,20 +14,11 @@ function circularArrayRotation(a, k, queries) {
         }
         return queriesArr
     }
-    // first for loop if for shifting array "k" many times to the right
-    console.log(`rewriter array: ${rewriterArr}, rewriter array[0,2]: ${rewriterArr[0][1]}`)
-    for(let x = k; x >= 0; x--){
-        for(let i=0; i<a.length; i++){
-            if(a[i - 1] === undefined){
-                a[i] = rewriterArr[0][(rewriterArr.length - 1)]
-            }else if(a[i + 1] === undefined){
-                a[i] = rewriterArr[0][(rewriterArr.length - 2)] 
-            }else{
-                a[i] = rewriterArr[0][i - 1]
-            }
+
+    for(let i=0; i<a.length; i++){
+        if(a[i + k] === undefined){
+            
         }
-        rewriterArr = []
-        rewriterArr.push(a)
     }
 
     // gets the value of the queries and returns
