4efa4ab161923a5de04c504a25160f93617f6c28
Broderick <zerothesu@gmail.com>
2024-09-09 23:20:12 -0600

    circular array rotation

--- circular-array-rotation.js
+++ circular-array-rotation.js
@@ -1,7 +1,11 @@
 function circularArrayRotation(a, k, queries) {
     let savedInt
     let rewriterArr = []
-    rewriterArr.push(a)
+    let newArr = []
+    for(let c=0; c<a.length; c++){
+        rewriterArr.push(a[c])
+        newArr.push(a[c])
+    }
     console.log(rewriterArr)
     let queriesArr = []
     if(k > a.length){
@@ -13,12 +17,53 @@ function circularArrayRotation(a, k, queries) {
             queriesArr.push(a[queries[g]])
         }
         return queriesArr
-    }
+    } 
 
-    for(let i=0; i<a.length; i++){
-        if(a[i + k] === undefined){
-            
+    for(let r=0; r<k; r++){
+        console.log('----------')
+        console.log('BEFORE LOOP')
+        console.log(`loop number ${r + 1}`)
+        console.log(`main array is ${a}`)
+        console.log(`newArr is ${newArr}`)
+        console.log(`rewriterArr is ${rewriterArr}`)
+        console.log(`--------------`)
+        for(let v=0; v<a.length; v++){
+            if(a[v - 1] === undefined){
+                console.log('first')
+                console.log(`setting ${a[v]} a[v] to ${rewriterArr[rewriterArr.length - 1]} rewrite last`)
+                console.log(`main array is ${a}`)
+                console.log(`newArr is ${newArr}`)
+                console.log(`rewriterArr is ${rewriterArr}`)
+                a[v] = rewriterArr[rewriterArr.length - 1]
+                newArr[v] = rewriterArr[rewriterArr.length - 1]
+            }else if(a[v + 1] === undefined){
+                console.log('last')
+                console.log(`setting ${a[v]} a[v] to ${rewriterArr[rewriterArr.length - 2]} rewrite second last`)
+                console.log(`main array is ${a}`)
+                console.log(`newArr is ${newArr}`)
+                console.log(`rewriterArr is ${rewriterArr}`)
+                a[v] = rewriterArr[rewriterArr.length - 2]
+                newArr[v] = rewriterArr[rewriterArr.length - 2]
+            }else{
+                console.log('lettuce and tomatoes')
+                console.log(`setting ${a[v]} a[v] to ${rewriterArr[v - 1]} rewriterArr[v - 1]`)
+                console.log(`main array is ${a}`)
+                console.log(`newArr is ${newArr}`)
+                console.log(`rewriterArr is ${rewriterArr}`)
+                a[v] = rewriterArr[v - 1]
+                newArr[v] = rewriterArr[v - 1]
+            }
         }
+        console.log('----------')
+        console.log('AFTER LOOP')
+        console.log(`loop number ${r + 1}`)
+        console.log(`main array is ${a}`)
+        console.log(`newArr is ${newArr}`)
+        console.log(`rewriterArr is ${rewriterArr}`)
+        console.log(`setting rewriterArr to newArr`)
+        console.log(`--------------`)
+        rewriterArr = newArr
+        
     }
 
     // gets the value of the queries and returns
