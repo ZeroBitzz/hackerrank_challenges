be4f25ee51fa6a383658c5017a1beb671539481b
Broderick <zerothesu@gmail.com>
2024-09-13 00:17:42 -0600

    completed circular array rotation

--- circular-array-rotation.js
+++ circular-array-rotation.js
@@ -1,31 +1,28 @@
 function circularArrayRotation(a, k, queries) {
     let savedInt
     let rewriterArr = []
+
+    // creates two of the inputed array for scanning
     for(let r=0; r<a.length; r++){
         rewriterArr.push(a[r])
     }
     for(let r=0; r<a.length; r++){
         rewriterArr.push(a[r])
     }
+
+    // puts dummy values in a so scanning still correlates properly with increased array size from above
     const firstALength = a.length
     for(let h=0; h<firstALength; h++){
         a.unshift('f')
     }
     let queriesArr = []
 
-    // removes excess loops from array and checks base case
+    // removes excess loops from array 
     if(k > a.length){
-        k -= a.length
-        console.log(`cut down k! k's new value ${k}`)
-    }else if(k === a.length){
-        // gets the value of the queries and returns
-        for(let g=0; g<queries.length; g++){
-            queriesArr.push(a[queries[g]])
-        }
-        console.log('flat output')
-        return queriesArr
-    } 
+        k = (k%a.length)
+    }
 
+    // sets new a values and breaks when it hits the dummy values
     for(let i=(a.length); i>0; i--){
         if(i === (a.length/2)){
             break
@@ -33,12 +30,14 @@ function circularArrayRotation(a, k, queries) {
         a[i-1] = rewriterArr[(i-1) - k]
     }
 
+    // gets the final array for the queries
     let newArr = []
     for(let l=0; l<a.length; l++){
         if(a[l] != 'f'){
             newArr.push(a[l])
         }
     }
+    
     // gets the value of the queries and returns
     for(let g=0; g<queries.length; g++){
         queriesArr.push(newArr[queries[g]])
@@ -46,6 +45,6 @@ function circularArrayRotation(a, k, queries) {
     return queriesArr
 }
 
-arr = [3,5,9,7,6,5,4,3,4]
+arr = [3,5,9,7]
 
 console.log(circularArrayRotation(arr, 2, [1,2]))
