5f8f9328685ea439aeb4207c50fb94968c7c682a
Broderick <zerothesu@gmail.com>
2024-09-15 22:41:35 -0600

    completed extra long factorials

--- extra-long-factorials.js
+++ extra-long-factorials.js
@@ -1,11 +1,18 @@
 function extraLongFactorials(n) {
-    let finalInt = n
+    // sets independent variable to change
+    // note: BigInt is for handling massive number calculations
+    let finalInt = BigInt(n)
+
+    // multiplies n * (n-1) * (n-2)....
     for(let i=1; i<n-1; i++){
-        // console.log(`multiplying ${n-i} with ${n-(i+1)}`)
-        finalInt *= (n-i)
-        // console.log(`finalInt is now ${finalInt}`)
+        finalInt *= BigInt(n-i)
     }
+
+    // gets rid of the n at the end of the number that was a result of the BigInt func
+    finalInt = finalInt.toString()
     console.log(finalInt)
 }
 
-extraLongFactorials(4)
+extraLongFactorials(30)
+
+// completed
