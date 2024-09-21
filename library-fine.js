3f65970ab539bc13e201cdcc2efccf4fcc0b5061
Broderick <zerothesu@gmail.com>
2024-09-20 23:08:53 -0600

    library-fine

---
+++ library-fine.js
@@ -0,0 +1,8 @@
+function libraryFine(d1, m1, y1, d2, m2, y2) {
+    if((d1 === d2) && (m1 === d2) && (y1 === y2)){
+        return 0
+    }else if((y1 < y2)){
+        return 0
+    }else if((m1 < m2) && (y1 <= y2))
+
+}
