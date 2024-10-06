5521a08b0e4c462d2e29e92e5f43c1e3ac47dc96
Broderick <zerothesu@gmail.com>
2024-10-05 21:46:59 -0600

    append and delete

--- append-and-delete.js
+++ append-and-delete.js
@@ -1,12 +1,18 @@
 function appendAndDelete(s, t, k) {
-    let changesNeeded = 0
+    let subtractorArr = []
+    if(s > t && k >= (s*2)){
+        return 'Yes'
+    }else if(t > s && k >= (t*2)){
+        return 'No'
+    }
     for(let i=0; i<s.length; i++){
         if(s[i] !== t[i]){
-            changesNeeded++
+            break
+        }else{
+            subtractorArr.push(s[i])
         }
     }
-    changesNeeded *= 2
-    if(changesNeeded <= k){
+    if((s.length-subtractorArr.length)*2 >= k){
         return 'Yes'
     }else{
         return 'No'
